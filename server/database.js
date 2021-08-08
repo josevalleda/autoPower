4fc77de2cad76777a1d74ce0294db255fbc133f8
Santiago <xsantysxx@gmail.com>
2021-08-08 14:56:29 -0500

    Terminado esperando correccion de errores

--- server/database.js
+++ server/database.js
@@ -8,6 +8,16 @@ const mysqlConnection = mysql.createConnection({
     multipleStatements: true
 });
 
+// const mysqlConnection = mysql.createConnection({
+//     host: 'us-cdbr-east-04.cleardb.com',
+//     user: 'b0adf71c13480f',
+//     password: '14152889',
+//     database: 'heroku_d29a844f755278c',
+//     multipleStatements: true
+// });
+
+//mysql://b0adf71c13480f:14152889@us-cdbr-east-04.cleardb.com/heroku_d29a844f755278c?reconnect=true
+
 mysqlConnection.connect(function(err) {
     if (err) {
         console.error(err);
